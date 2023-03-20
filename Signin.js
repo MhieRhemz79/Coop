d74982e15b509ed72c3130f40d2cc8c0da354674
Remedios Sibug <112613658+MhieRhemz79@users.noreply.github.com>
2023-03-21 06:52:27 +0800

    Add files via upload

---
+++ Signin.js
@@ -0,0 +1,46 @@
+import React, { useState }from 'react'
+import './signin.css'
+import Navbar from '../../components/Navbar';
+import Sidebar from '../../components/Sidebar';
+import Footer from '../../components/Footer';
+
+const Signin = () => {
+
+  const [popupStyle, showPopup] = useState("hide");
+
+  const popup =() => {
+    showPopup("login-popup")
+    setTimeout(() => showPopup("hide"), 3000)
+  }
+
+  return (
+
+    <>
+    <Sidebar />
+    <Navbar />
+
+    <page>
+    <div className="cover">
+      <h1>Signin</h1>
+      <input type="text" placeholder="username/email address" />
+      <input type="password" placeholder="password" />
+
+      <div className="login-btn" onClick={popup}>Signin</div>
+      <p className="text">*PEN-COOP*</p>
+
+  
+
+    <div className={popupStyle}>
+      <h3>Login Failed</h3>
+      <p>Username or Password Incorrect</p>
+    </div>
+    </div>
+    </page>
+    <Footer />
+
+      </>
+
+  );
+}
+
+export default Signin;
