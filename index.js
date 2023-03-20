54821d7a2b297231813cb75b26d80bcbbaf8bbdc
Remedios Sibug <112613658+MhieRhemz79@users.noreply.github.com>
2023-03-21 06:49:12 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Video from '../../videos/video2.mp4';
+import { HeroContainer, HeroBg, VideoBg } from './HeroElements';
+const HeroSection = () => {
+  return (
+    <HeroContainer>
+        <HeroBg>
+            <VideoBg autoPlay loop src={Video} type='video2/.mp4' />
+        </HeroBg>
+    </HeroContainer>
+  );
+};
+
+export default HeroSection;
