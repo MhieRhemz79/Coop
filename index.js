0b75a140a0ba1460d415c7890644373478cb78b9
Remedios Sibug <112613658+MhieRhemz79@users.noreply.github.com>
2023-03-21 18:07:11 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import Video from '../../videos/video2.mp4';
+import { HeroContainer, HeroBg, VideoBg } from './HeroElements';
 
+const HeroSection = () => {
+  return (
+    <HeroContainer>
+        <HeroBg>
+            <VideoBg autoPlay loop src={Video} type='video2/.mp4' />
+        </HeroBg>
+    </HeroContainer>
+  );
+};
+
+
+export default HeroSection;
